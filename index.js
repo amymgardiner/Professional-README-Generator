5f110ba6d6e4c4831be9f05975acfc1803610aaa
Amy Gardiner <amymgardiner@gmail.com>
2022-07-23 13:26:44 -0500

    removed challenge TO DO comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
 const questions = [
     // project title
     {
@@ -126,7 +124,7 @@ const questions = [
     },
 ];
 
-// // TODO: Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(`./dist/${fileName}`, data, err => {
         if (err) {
@@ -137,12 +135,12 @@ function writeToFile(fileName, data) {
     });
 };
 
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     return inquirer.prompt(questions)
 };
 
-// Function call to initialize app
+// function call to initialize app
 init()
     .then(userAnswers => generateMarkdown(userAnswers))
     .then(pageMD => writeToFile('README.md', pageMD))
